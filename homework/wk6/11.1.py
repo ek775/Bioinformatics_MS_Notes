9ca0dfda2191e551dd7067a04e999093e10167a9
ek775 <eliotkmiec@gmail.com>
2023-09-29 15:08:46 -0400

    fixed file parsing issue, created EDA notebook for reference

--- homework/wk6/11.1.py
+++ homework/wk6/11.1.py
@@ -14,7 +14,7 @@ values of a specific gene overall, and within each sample category.
 
 # loading packages
 import csv
-import sys
+#import sys
 
 #try: 
     #file = sys.argv[1]
@@ -22,9 +22,40 @@ import sys
     #print("Missing File Argument")
     #exit(1)
 
-file = "~/Documents/ms_bioinformatics/python_intro/homework/wk6/data.csv"
+##################################################################
+# DEFINING MATHEMATICAL FUNCTIONS
+##################################################################
 
+def mean():
+    pass
+
+def average():
+    pass
+
+##################################################################
+# LOAD AND PARSE DATA
+##################################################################
+
+file = 'data.csv'
 reader = csv.DictReader(open(file))
+
+#put data into 2D-list for access
+#1D is each row, 2D is the dictionary for each
+shitty_data_frame = [r for r in reader]
+#print(shitty_data_frame)
+
+### collect data from each column
+
+#initialize dict for aggregation
+columns = [i for i in shitty_data_frame[1].keys()]
+init_zeros = [0 for i in range(len(columns))]
+column_aggregation = dict(zip(columns,init_zeros))
+print(column_aggregation)
+
+#aggregate data
+#for row in shitty_data_frame:
+    #for dict in row:
+
 
 
 
